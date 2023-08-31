d68096cf9b9fdb34d2c2dcce649bdbe00d95f4ff
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 18:06:35 +0530

    First Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -24,7 +24,7 @@ const LoginForm: React.FC = () => {
         setError(response.message);
       }
     } catch (error) {
-      setError('Failed to login user');
+      setError(error.message);
     }
   };
 
