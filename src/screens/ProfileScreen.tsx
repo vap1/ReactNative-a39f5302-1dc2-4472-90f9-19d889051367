547836bd94a17049a670bbafefc91361bbf11f34
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 17:34:03 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,15 +1,15 @@
 
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, Button, Text } from 'react-native';
 import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
-import { getUserProfile, updateUserProfile } from '../apis/UserProfileApi';
+import getUserProfile from '../apis/UserProfileApi';
+import updateUserProfile from '../apis/UserProfileUpdateApi';
 
 const ProfileScreen: React.FC = () => {
   const [name, setName] = useState('');
   const [contactInfo, setContactInfo] = useState('');
   const [address, setAddress] = useState('');
   const [profilePicture, setProfilePicture] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
@@ -19,61 +19,66 @@ const ProfileScreen: React.FC = () => {
   const fetchUserProfile = async () => {
     try {
       const request: UserProfileRequest = {
-        token: 'YOUR_JWT_TOKEN', // Replace with actual JWT token
+        token: 'YOUR_JWT_TOKEN', // Replace with the actual JWT token
       };
+
       const response: UserProfileResponse = await getUserProfile(request);
-      setName(response.user.name);
-      setContactInfo(response.user.contactInfo || '');
-      setAddress(response.user.address || '');
-      setProfilePicture(response.user.profilePicture || '');
-      setIsLoading(false);
+
+      const { user } = response;
+      setName(user.name);
+      setContactInfo(user.contactInfo || '');
+      setAddress(user.address || '');
+      setProfilePicture(user.profilePicture || '');
     } catch (error) {
-      setError('Failed to fetch user profile');
-      setIsLoading(false);
+      setError('Failed to retrieve user profile');
     }
   };
 
-  const handleSaveChanges = async () => {
+  const handleProfileUpdate = async () => {
     try {
       const request: UserProfileUpdateRequest = {
-        token: 'YOUR_JWT_TOKEN', // Replace with actual JWT token
+        token: 'YOUR_JWT_TOKEN', // Replace with the actual JWT token
         name,
         contactInfo,
         address,
         profilePicture,
       };
+
       const response: UserProfileUpdateResponse = await updateUserProfile(request);
-      setError('');
-      // Show success message or perform any other actions
+
+      if (response.success) {
+        // Profile update successful
+      } else {
+        setError(response.message);
+      }
     } catch (error) {
       setError('Failed to update user profile');
     }
   };
 
-  if (isLoading) {
-    return (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
-
   return (
     <View>
-      <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} />
-
-      <Text>Contact Info:</Text>
-      <TextInput value={contactInfo} onChangeText={setContactInfo} />
-
-      <Text>Address:</Text>
-      <TextInput value={address} onChangeText={setAddress} />
-
-      <Text>Profile Picture:</Text>
-      <Image source={{ uri: profilePicture }} style={{ width: 100, height: 100 }} />
-
-      <Button title="Save Changes" onPress={handleSaveChanges} />
-
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        placeholder="Contact Info"
+        value={contactInfo}
+        onChangeText={setContactInfo}
+      />
+      <TextInput
+        placeholder="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <TextInput
+        placeholder="Profile Picture"
+        value={profilePicture}
+        onChangeText={setProfilePicture}
+      />
+      <Button title="Save Changes" onPress={handleProfileUpdate} />
       {error ? <Text>{error}</Text> : null}
     </View>
   );
