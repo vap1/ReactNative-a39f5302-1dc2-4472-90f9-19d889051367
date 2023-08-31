6e19b910eea26cd912c87817fd4aa38a8cb85db7
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 17:29:36 +0530

    First Commit

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import RegistrationScreen from '../screens/RegistrationScreen';
+import LoginScreen from '../screens/LoginScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import AdminUserDetailsScreen from '../screens/AdminUserDetailsScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
