b83b6cc788d1811905c01e6f9532c46c31bcbcc2
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 17:38:50 +0530

    First Commit

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -24,7 +24,7 @@ const LoginScreen: React.FC = () => {
         setError(response.message);
       }
     } catch (error) {
-      setError('Failed to login user');
+      setError(error.message);
     }
   };
 
@@ -37,9 +37,9 @@ const LoginScreen: React.FC = () => {
       />
       <TextInput
         placeholder="Password"
-        secureTextEntry
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
       />
       <Button title="Login" onPress={handleLogin} />
       {error ? <Text>{error}</Text> : null}
