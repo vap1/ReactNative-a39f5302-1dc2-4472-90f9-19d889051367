0baf6be7d539a798bbf935352e27e4b4306b8024
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 17:29:27 +0530

    First Commit

---
+++ src/apis/UserProfileUpdateApi.ts
@@ -0,0 +1,14 @@
+
+import axios from 'axios';
+import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
+
+const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
+  try {
+    const response = await axios.put('/api/profile', request);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export default updateUserProfile;
